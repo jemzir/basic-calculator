1ffc89a7b6d3240e8e2a81038af8a9dd917a4c79
jemz <nghiemj23@gmail.com>
2022-04-21 00:13:31 -0700

    add buttons that get clicked to display and clear button

--- my-js-file.js
+++ my-js-file.js
@@ -10,12 +10,27 @@ let calculatorItems ="()% 789+456x123-0.=+";
 
 numpad.childNodes[3].textContent = 'AC';
 
-console.log(numpad.childNodes[1]);
-
 for (i = 0; i < 20; i +=1) {
     if (!numpad.childNodes[i].textContent) {
         numpad.childNodes[i].textContent = calculatorItems[i];
     }
 }
 
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns.length);
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.textContent === 'AC') {
+            display.textContent = '';
+            return
+        }
+        display.textContent += e.target.textContent;
+    })
+})
+
 const display = document.querySelector('#display');
+display.textContent = '';
+
+console.log(display.textContent);
